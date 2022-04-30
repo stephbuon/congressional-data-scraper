dac852d5899d269876426232c0b9af3afdbef20a
alexanderr <localtoasted@pm.me>
2022-04-30 13:02:17 -0500

    new scraper code

---
+++ congressional_scraper.py
@@ -0,0 +1,81 @@
+import argparse
+from typing import Optional
+
+BASE_URL = 'https://congress.gov'
+SEARCH_URL = f'{BASE_URL}/search'
+
+from urllib.parse import quote_plus, urlencode
+import json
+import requests
+
+from bs4 import BeautifulSoup
+
+import speaker_scraper
+
+
+def create_query(search_term: str, congress: Optional[str] = None):
+    q = {
+        "source": "congrecord",
+        "search": search_term,
+    }
+
+    if congress is not None:
+        q["congress"] = int(congress)
+
+    return json.dumps(q)
+
+
+def scrape_search_results(search_term):
+    url_params = {
+        'q': create_query(search_term),
+        'pageSize': 100,
+        'page': 1,
+    }
+    url = f'{SEARCH_URL}?{urlencode(url_params)}'
+    print(f'Search url: {url}')
+
+    response = requests.get(url)
+    print('Parsing search page...')
+    page = BeautifulSoup(response.text, 'html.parser')
+    body = page.body
+    search_results = body.find_all('span', class_='congressional-record-heading')
+    print('Number of results: %d' % len(search_results))
+
+    for result in search_results:
+        tag = result.find('a')
+        result_href = tag.get('href')
+        scrape_record(f'{BASE_URL}{result_href}')
+        break
+
+
+def scrape_record(url):
+    response = requests.get(url)
+    page = BeautifulSoup(response.text, 'html.parser')
+    txt_link_parent = page.find('li', class_='full-text-link')
+    txt_link = next(txt_link_parent.children).get('href')
+    print(f'Fetching record from {BASE_URL}{txt_link}...')
+
+    response = requests.get(f'{BASE_URL}{txt_link}')
+    page = BeautifulSoup(response.text, 'html.parser')
+
+    record_text = page.find('pre').text
+    for match in speaker_scraper.scrape(record_text):
+        print(match)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('search',
+                        help='Return congressional records containing this term.')
+    # parser.add_argument('-fy', '--start_year',
+    #                     help='The starting date in your date range (it does not matter if the larger or smaller year comes first).')
+    # parser.add_argument('-ly', '--end_year',
+    #                     help='The ending date in your date range (it does not matter if the smaller or larger year comes first).')
+
+    try:
+       args = parser.parse_args()
+    except IndexError:
+       exit('Congressional Data Scraper takes three arguments: search term, first year, last year. Please see github.com/stephbuon/congressional-data-scraper for more information')
+
+    scrape_search_results(args.search)
---
+++ speaker_scraper.py
@@ -0,0 +1,23 @@
+import re
+
+SPEAKER_REGEX = re.compile(r'(?:^  +M[rs]. [a-zA-Z]+[A-Z ]+\.)|(?:^  +The [A-Z ]+.)', re.MULTILINE)
+
+
+def scrape(text):
+    current_speaker = None
+    speech_start = None
+    speech_end = None
+
+    for m in re.finditer(SPEAKER_REGEX, text):
+        name_start = m.start(0)
+        name_end = m.end(0)
+
+        if current_speaker is not None:
+            speech_end = name_start - 1
+            yield current_speaker, text[speech_start:speech_end]
+
+        speech_start = name_end + 1
+        current_speaker = text[name_start:name_end].strip()[:-1]
+
+    if current_speaker is not None:
+        yield current_speaker, text[speech_start:]
