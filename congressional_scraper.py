d07f9fc05ad9356a78daa00fa486e3dfda69e4f2
alexanderr <localtoasted@pm.me>
2022-08-06 12:55:03 -0700

    fix recursion depth issue on errors

--- congressional_scraper.py
+++ congressional_scraper.py
@@ -6,7 +6,7 @@ BASE_URL = 'https://congress.gov'
 SEARCH_URL = f'{BASE_URL}/search'
 PAGE_SIZE = 100
 DEFAULT_CONGRESS = list(range(105, 117 + 1))
-RETRY_DELAY = 10
+DEFAULT_RETRY_DELAY = 100
 
 TOO_MANY_REQUESTS = 429
 
@@ -39,17 +39,19 @@ def create_query(search_term: str, congress: Optional[List[str]] = None):
 def fetch_retry_time(response: requests.Response):
     retry_time = response.headers.get('Retry-After', None)
     if retry_time is None:
-        retry_time = RETRY_DELAY
+        print('No retry time header, using default.')
+        retry_time = DEFAULT_RETRY_DELAY
     else:
         try:
             retry_time = int(retry_time)
             print('Retry-After time from API is: %d' % retry_time)
         except ValueError:
-            retry_time = RETRY_DELAY
+            print('Failed to parse retry time header, using default.')
+            retry_time = DEFAULT_RETRY_DELAY
     return retry_time
 
 
-def scrape_search_results(search_term, page_num=1, max_results=9999, retries=1):
+def scrape_search_results(search_term, max_results, page_num=1, retries=3):
     url_params = {
         'q': create_query(search_term, congress=DEFAULT_CONGRESS),
         'pageSize': PAGE_SIZE,
@@ -57,6 +59,7 @@ def scrape_search_results(search_term, page_num=1, max_results=9999, retries=1):
     }
     url = f'{SEARCH_URL}?{urlencode(url_params)}'
     print(f'Search url: {url}')
+    print('Page number is: %d' % page_num)
 
     response = requests.get(url)
 
@@ -64,14 +67,16 @@ def scrape_search_results(search_term, page_num=1, max_results=9999, retries=1):
         response.raise_for_status()
     except requests.HTTPError as e:
         print('Failed to fetch initial search url. Reason: ' + str(e))
-        if response.status_code == TOO_MANY_REQUESTS:
+        if retries and response.status_code == TOO_MANY_REQUESTS:
             retry_time = fetch_retry_time(response)
             time.sleep(retry_time)
-            yield from scrape_search_results(search_term, page_num=page_num, max_results=max_results, retries=retries - 1)
-        if retries:
+            return scrape_search_results(search_term, page_num=page_num, max_results=max_results, retries=retries - 1)
+        elif retries:
             print('Retrying...')
-            time.sleep(RETRY_DELAY)
-            yield from scrape_search_results(search_term, page_num=page_num, max_results=max_results, retries=retries - 1)
+            time.sleep(DEFAULT_RETRY_DELAY)
+            return scrape_search_results(search_term, page_num=page_num, max_results=max_results, retries=retries - 1)
+        else:
+            print('Out of retries, skipping page...')
         return
 
     print('Parsing search page...')
@@ -80,8 +85,8 @@ def scrape_search_results(search_term, page_num=1, max_results=9999, retries=1):
     search_results = body.find_all('span', class_='congressional-record-heading')
     print('Number of results: %d' % len(search_results))
 
-    for result in search_results:
-        tag = result.find('a')
+    for search_result_span in search_results:
+        tag = search_result_span.find('a')
         result_href = tag.get('href')
         try:
             yield from scrape_record(f'{BASE_URL}{result_href}')
@@ -92,13 +97,13 @@ def scrape_search_results(search_term, page_num=1, max_results=9999, retries=1):
         max_results -= 1
         if not max_results:
             break
+    #
+    # if max_results and search_results:
+    #     print('Continuing to next page...')
+    #     yield from scrape_search_results(search_term, page_num + 1, max_results)
 
-    if max_results and search_results:
-        print('Continuing to next page...')
-        yield from scrape_search_results(search_term, page_num + 1, max_results)
 
-
-def scrape_record(url, retries=1):
+def scrape_record(url, retries=3):
     print('scrape_record', url)
     response = requests.get(url)
 
@@ -106,15 +111,17 @@ def scrape_record(url, retries=1):
         response.raise_for_status()
     except requests.HTTPError as e:
         print('Failed to scrape record. Reason: ' + str(e))
-        if response.status_code == TOO_MANY_REQUESTS:
+        if retries and response.status_code == TOO_MANY_REQUESTS:
             retry_time = fetch_retry_time(response)
             time.sleep(retry_time)
             return scrape_record(url, retries=retries - 1)
-
-        if retries:
+        elif retries:
             print('Retrying...')
-            time.sleep(RETRY_DELAY)
+            time.sleep(DEFAULT_RETRY_DELAY)
             return scrape_record(url, retries=retries - 1)
+        else:
+            print('Out of retries, skipping record...')
+            return
 
     page = BeautifulSoup(response.text, 'html.parser')
     main_wrapper = page.find('div', class_='main-wrapper')
@@ -146,7 +153,7 @@ def scrape_record(url, retries=1):
     return scrape_txt_record(txt_link, url, date, title)
 
 
-def scrape_txt_record(txt_link, record_url, record_date, record_title, retries=1):
+def scrape_txt_record(txt_link, record_url, record_date, record_title, retries=3):
     print(f'Fetching record from {BASE_URL}{txt_link}...')
 
     response = requests.get(f'{BASE_URL}{txt_link}')
@@ -155,14 +162,17 @@ def scrape_txt_record(txt_link, record_url, record_date, record_title, retries=1
         response.raise_for_status()
     except requests.HTTPError as e:
         print('Failed to scrape TXT record link. Reason: ' + str(e))
-        if response.status_code == TOO_MANY_REQUESTS:
+        if retries and response.status_code == TOO_MANY_REQUESTS:
             retry_time = fetch_retry_time(response)
             time.sleep(retry_time)
             return scrape_txt_record(txt_link, record_url, record_date, record_title, retries=retries - 1)
-        if retries:
+        elif retries:
             print('Retrying...')
-            time.sleep(RETRY_DELAY)
+            time.sleep(DEFAULT_RETRY_DELAY)
             return scrape_txt_record(txt_link, record_url, record_date, record_title, retries=retries - 1)
+        else:
+            print('Out of retries, skipping TXT data...')
+            return
 
     page = BeautifulSoup(response.text, 'html.parser')
 
@@ -183,21 +193,45 @@ if __name__ == '__main__':
                         help='Return congressional records containing this term.')
     parser.add_argument('output_file',
                         help='Return congressional records containing this term.')
+    parser.add_argument('-r', '--result-count',
+                        help='Max number of results you want',
+                        default=9000, type=int, nargs='?')
     # parser.add_argument('-fy', '--start_year',
     #                     help='The starting date in your date range (it does not matter if the larger or smaller year comes first).')
     # parser.add_argument('-ly', '--end_year',
     #                     help='The ending date in your date range (it does not matter if the smaller or larger year comes first).')
     args = parser.parse_args()
-    results = []
+    max_result_count = args.result_count
+    page_count = 0
+    total_result_count = 0
 
-    for url, date, title, speaker, text in scrape_search_results(args.search_term):
-        text = text.replace('\n', ' ').replace('\t', ' ')
-        results.append({"url": url, "date": date, "title": title, "speaker": speaker, "text": text})
+    # Reset/create file
+    with open(args.output_file, 'w+') as f:
+        pass
 
-    if not results:
-        print('No results scraped. Exiting...')
-        exit()
+    while max_result_count:
+        num_page_results = 0
+        page_results = []
+        page_count += 1
+        for result in scrape_search_results(args.search_term, max_result_count, page_num=page_count):
+            if result is None:
+                continue
+            url, date, title, speaker, text = result
+            text = text.replace('\n', ' ').replace('\t', ' ')
+            page_results.append({"url": url, "date": date, "title": title, "speaker": speaker, "text": text})
+            num_page_results += 1
+            max_result_count -= 1
 
-    df = pd.DataFrame(results)
-    df.set_index("url", inplace=True)
-    df.to_csv(args.output_file, sep='|')
+            if not max_result_count:
+                break
+
+        if not num_page_results:
+            break
+
+        df = pd.DataFrame(page_results)
+        df.set_index("url", inplace=True)
+        df.to_csv(args.output_file, sep='|', mode='a', header=page_count == 1)
+
+        total_result_count += num_page_results
+
+    print('Scraped: %d pages, %d individual records.' % (page_count, total_result_count))
